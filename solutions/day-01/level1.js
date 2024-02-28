d6b7fdbaf57e06d6184ca164b8b5257e80d9952b
Aryan Saji <aryansaji3103@gmail.com>
2024-02-28 22:30:45 +0530

    Exercise Day1 Complete

---
+++ solutions/day-01/level1.js
@@ -0,0 +1,27 @@
+//What is React
+
+/*
+1. What is React?
+    -React is a JavaScript Library for building reusable User Interface(UI).
+    -It is used to build single page applications.
+    -React allows us to create reusable UI components.
+
+2. What is a Library?
+    -It is a collection of pre-written code that you can use to perform specific tasks.
+
+3. What is a single page application?
+    -An application which has only one HTML page.
+
+4. What is a component?
+    -It is an identifiable part of a larger program or construction.
+
+5. What is the latest version of React?
+    -React 18
+
+6. What is DOM?
+    -Document Object Model
+
+7. What is React Virtual DOM?
+    -React uses Virtual DOM exists which is like a lightweight copy of the actual DOM(a virtual representation of the DOM). 
+*/
+
